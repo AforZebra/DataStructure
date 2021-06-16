5106d66bd847e2f52e3e214bb392ca4b2f1144aa
allipurHari <37536117+allipurHari@users.noreply.github.com>
2021-06-16 18:26:15 +0530

    Create maxDepth.cpp

---
+++ Tree/maxDepth.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/535
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root)return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
