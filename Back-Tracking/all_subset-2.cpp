1e3a126dc0bd4a7b7fb7bd8289c23d66ce75ecea
allipurHari <37536117+allipurHari@users.noreply.github.com>
2021-07-12 23:14:22 +0530

    Create all_subset-2.cpp

---
+++ Back-Tracking/all_subset-2.cpp
@@ -0,0 +1,26 @@
+// https://www.interviewbit.com/old/problems/subsets-ii/
+
+void call(int idx, vector<int>A, int n, vector<vector<int>> &result, vector<int>v, string s, unordered_set<string>&us){
+    if(us.find(s) == us.end()){
+        us.insert(s);
+        result.push_back(v);
+    }
+    else return;
+    for(int i = idx;i < n;i++){
+        v.push_back(A[i]);
+        int strSize = s.size();
+        s += to_string(A[i]);
+        call(i+1, A, n, result, v, s, us);
+        s = s.substr(0,strSize);
+        v.pop_back();
+    }
+}
+
+vector<vector<int> > Solution::subsetsWithDup(vector<int> &A) {
+    vector<vector<int> > result;
+    vector<int>v;
+    unordered_set<string>us;
+    sort(A.begin(),A.end());
+    call(0, A, A.size(), result, v, "", us);
+    return result;
+}
