c19d8dbc979f60208fd56f2f8876a6f3ba839cf2
allipurHari <37536117+allipurHari@users.noreply.github.com>
2021-07-13 13:21:21 +0530

    Create combination_sum-2.cpp

---
+++ Back-Tracking/combination_sum-2.cpp
@@ -0,0 +1,25 @@
+// https://www.interviewbit.com/old/problems/combination-sum-ii/
+
+void call(int idx, int n, int sum, vector<int>A, vector<int>v, set<vector<int>>&s){
+    if(sum == 0){
+        s.insert(v);
+        return;
+    }
+    if(sum < 0)return;
+    for(int i = idx;i < n;i++){
+        v.push_back(A[i]);
+        call(i+1,n,sum-A[i],A,v,s);
+        v.pop_back();
+    }
+}
+
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    sort(A.begin(),A.end());
+    vector<int>v;
+    vector<vector<int>>ans;
+    set<vector<int>>s;
+    call(0,A.size(),B,A,v,s);
+    for(auto itr = s.begin();itr != s.end();itr++)
+        ans.push_back(*itr);
+    return ans;
+}
