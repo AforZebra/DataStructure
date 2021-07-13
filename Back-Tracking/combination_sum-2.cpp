9d8a924254e7ad641f9f5f483fc2dd3365d34950
allipurHari <37536117+allipurHari@users.noreply.github.com>
2021-07-13 13:26:35 +0530

    Update combination_sum-2.cpp

--- Back-Tracking/combination_sum-2.cpp
+++ Back-Tracking/combination_sum-2.cpp
@@ -23,3 +23,31 @@ vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
         ans.push_back(*itr);
     return ans;
 }
+
+
+
+// Solution 2
+
+void call(int idx, int n, int sum, vector<int>A, vector<int>v, set<vector<int>>&s){
+    if(sum == 0){
+        s.insert(v);
+        return;
+    }
+    if(sum < 0 || idx >= n)return;
+    v.push_back(A[idx]);
+    call(idx+1,n,sum-A[idx],A,v,s);
+    v.pop_back();
+    call(idx+1,n,sum,A,v,s);
+}
+
+vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
+    sort(A.begin(),A.end());
+    vector<int>v;
+    vector<vector<int>>ans;
+    set<vector<int>>s;
+    call(0,A.size(),B,A,v,s);
+    for(auto itr : s)
+        ans.push_back(itr);
+    return ans;
+}
+
