4c01ad28165946239275c231f66469555a21599f
allipurHari <37536117+allipurHari@users.noreply.github.com>
2021-07-24 13:58:43 +0530

    Update longest_palindromic_subsequence.cpp

--- DP/longest_palindromic_subsequence.cpp
+++ DP/longest_palindromic_subsequence.cpp
@@ -16,3 +16,22 @@ int Solution::solve(string A) {
     vector<vector<int>>dp(A.size(),vector<int>(A.size(),-1));
     return palindrome(0,A.size()-1,A,dp);
 }
+
+// bottom-up
+
+int Solution::solve(string A) {
+    string B = A;
+    reverse(B.begin(),B.end());
+    int n = A.size();
+    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= n;j++){
+            if(A[i-1] == B[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[n][n];
+}
+
